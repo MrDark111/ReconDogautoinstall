888570c313087681a26fff2ca174107021ea6d65
MrDark111 <73544166+MrDark111@users.noreply.github.com>
2021-06-22 11:28:17 +0530

    Add files via upload

---
+++ ReconDogautoinstall.sh
@@ -0,0 +1,55 @@
+echo
+
+clear
+
+cd
+
+clear
+
+termux-setup-storage
+
+clear
+
+apt update 
+
+apt upgrade
+
+pkg install git
+
+pkg install python python2
+
+clear
+
+cd
+
+rm -rf ReconDog
+
+rm -rf reconDog
+
+rm -rf Recondog 
+
+rm -rf recondog 
+
+clear
+
+cd
+
+git clone https://github.com/UltimateHackers/ReconDog 
+
+clear 
+
+cd ReconDog
+
+python dog 
+
+cd
+
+clear
+
+rm -rf ReconDogautoinstall 
+
+cd
+
+clear
+
+echo CODE BY MR DARK 
